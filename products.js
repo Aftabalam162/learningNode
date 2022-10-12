af6560f830e5d349eba4fd06a60f175c609685d9
Aftabalam37 <Itsaftabalam37@gmail.com>
2022-10-12 21:10:19 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,9 @@
+const products = [
+    {id: 0, title: 'iPhone 9', price: 64000.00, discount: 3, desc: 'It is one of the most sought after mobile phone in the market. You better get it!'},
+    {id: 1, title: 'Lenovo Ideacenter', price: 32000.00, discount: 4, desc:'Good computer for the students and light office work. Works fine for the average budget developers.'},
+    {id: 2, title: 'MiBook', price: 29000.00, discount: 4, desc: 'Brand new laptop and have the latest features but comes with the heating problems'},
+    {id: 3, title: 'Oppo F19', price: 16000.00, discount: 3, desc: 'Good camera quality phone but expensive for the price sensitive customers'},
+    {id: 4, title: 'MacBook Pro', price: 85000.00, discount: 5, desc: 'Top brand and good specs. Best suited for programming and video editing purpose'},
+];
+
+module.exports = products;
